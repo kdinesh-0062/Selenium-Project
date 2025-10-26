3cd5378c9263b72831372234e9dd20b8bff0bed9
kdinesh-0062 <kdinesh0062@gmail.com>
2025-10-27 00:35:25 +0530

    testing2

--- src/Trickies/AddNewTest.java
+++ src/Trickies/AddNewTest.java
@@ -2,4 +2,14 @@ package Trickies;
 
 public class AddNewTest {
 
+public static void main(String[] args) {
+		String str = "ABCXYZ";
+	int middle = str.length() / 2;
+	String firstHalf = str.substring(0, middle);
+	String secondHalf = str.substring(middle);
+	String reversedSecondHalf = new StringBuilder(secondHalf).reverse().toString();
+	String result = reversedSecondHalf + firstHalf;
+	System.out.println("Output: " + result);
+}
+
 }
